f4e500b05804e06f905e521d50f4e0967edb7e2d
Prathyusha <reddyprathyusha8199@gmail.com>
2025-01-19 13:26:31 +0530

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import  "./Birthday.css"
+
+const Birthday = () => {
+  return (
+    <div className='box'>
+      <div className='box2'>
+      <h2>6 birthdays today</h2>
+      <div className='birth1'>
+        <div className='img'>
+        <img src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg" alt="" />
+        </div>
+        <div className='name'>
+          
+        <h4>Bertie Yates</h4>
+        <p>29 years</p>
+        </div>
+      </div>
+      <br/>
+      <div className='birth1'>
+        <div className='img'>
+        <img src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg" alt="" />
+        </div>
+        <div className='name'>
+          
+        <h4>Hester Hogan</h4>
+        <p>32 years</p>
+        </div>
+      </div>
+      <br/>
+      <div className='birth1'>
+        <div className='img'>
+        <img src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg" alt="" />
+        </div>
+        <div className='name'>
+          
+        <h4>Larry Little</h4>
+        <p>36 years</p>
+        </div>
+      </div>
+      <br/>
+      <div className='birth1'>
+        <div className='img'>
+        <img src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg" alt="" />
+        </div>
+        <div className='name'>
+          
+        <h4>Sean Walsh</h4>
+        <p>38 years</p>
+        </div>
+      </div>
+      <br/>
+      <div className='birth1'>
+        <div className='img'>
+        <img src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg" alt="" />
+        </div>
+        <div className='name'>
+          
+        <h4>Lola Gardner</h4>
+        <p>38 years</p>
+        </div>
+      </div>
+      <br/>
+      <div className='birth1'>
+        <div className='img'>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuwG_B3F4VCvPO59lsNlP6rNt6_hfaGcq9rA&s" alt="" />
+        </div>
+        <div className='name'>
+          
+        <h4>Sudesh Gowda</h4>
+        <p>28 years</p>
+        </div>
+      </div>
+      <br/>
+      <div className='btn'>
+      <button >Clear All</button>
+      </div>
+      
+      </div>
+      
+    </div>
+  )
+}
+
+export default Birthday
